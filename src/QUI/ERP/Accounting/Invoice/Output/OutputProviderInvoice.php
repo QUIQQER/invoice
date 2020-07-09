bb6b4ad7d8228b883ee9124582a1befe47ee5f55
Patrick Müller <p.mueller@pcsg.de>
2020-07-09 12:05:36 +0200

    refactor: OutputProvider -> changed some methods to reflect new interface

--- src/QUI/ERP/Accounting/Invoice/Output/OutputProviderInvoice.php
+++ src/QUI/ERP/Accounting/Invoice/Output/OutputProviderInvoice.php
@@ -31,6 +31,21 @@ class OutputProviderInvoice implements OutputProviderInterface
     }
 
     /**
+     * Get title for the output entity
+     *
+     * @param Locale $Locale (optional) - If ommitted use \QUI::getLocale()
+     * @return mixed
+     */
+    public static function getEntityTypeTitle(Locale $Locale = null)
+    {
+        if (empty($Locale)) {
+            $Locale = QUI::getLocale();
+        }
+
+        return $Locale->get('quiqqer/invoice', 'OutputProvider.entity.title.Invoice');
+    }
+
+    /**
      * Get download filename (without file extension)
      *
      * @param string|int $entityId
@@ -218,12 +233,11 @@ class OutputProviderInvoice implements OutputProviderInterface
      * Get e-mail body when document is sent via mail
      *
      * @param string|int $entityId
-     * @param string $outputHtml
      * @return string
      *
      * @throws QUI\Exception
      */
-    public static function getMailBody($entityId, string $outputHtml)
+    public static function getMailBody($entityId)
     {
         $Invoice  = InvoiceUtils::getInvoiceByString($entityId);
         $Customer = $Invoice->getCustomer();
@@ -238,8 +252,7 @@ class OutputProviderInvoice implements OutputProviderInterface
         return QUI::getLocale()->get('quiqqer/invoice', 'invoice.send.mail.message', [
             'invoiceId' => $Invoice->getId(),
             'user'      => $user,
-            'address'   => $Customer->getAddress()->render(),
-            'invoice'   => $outputHtml
+            'address'   => $Customer->getAddress()->render()
         ]);
     }
 }
