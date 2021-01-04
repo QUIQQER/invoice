7db6ea23a83d271299ada4958ea344b67ec7421d
Henning Leutz <leutz@pcsg.de>
2021-01-04 16:29:43 +0100

    refactor: used QUI\ERP\Customer\Utils::getInstance()->getPaymentTimeForUser for time for payment

--- ajax/invoices/temporary/getTimeForPayment.php
+++ ajax/invoices/temporary/getTimeForPayment.php
@@ -10,24 +10,7 @@
 QUI::$Ajax->registerFunction(
     'package_quiqqer_invoice_ajax_invoices_temporary_getTimeForPayment',
     function ($uid) {
-        try {
-            $User = QUI::getUsers()->get($uid);
-        } catch (QUI\Exception $Exception) {
-            // default time for payment
-            return QUI\ERP\Accounting\Invoice\Settings::getInstance()->get('invoice', 'time_for_payment');
-        }
-
-        $permission = $User->getPermission('quiqqer.invoice.timeForPayment', 'maxInteger');
-
-        if (empty($permission)) {
-            $permission = QUI\ERP\Accounting\Invoice\Settings::getInstance()->get('invoice', 'time_for_payment');
-        }
-
-        if ($User->getAttribute('quiqqer.erp.customer.payment.term')) {
-            $permission = $User->getAttribute('quiqqer.erp.customer.payment.term');
-        }
-
-        return $permission;
+        return QUI\ERP\Customer\Utils::getInstance()->getPaymentTimeForUser($uid);
     },
     ['uid'],
     'Permission::checkAdminUser'
